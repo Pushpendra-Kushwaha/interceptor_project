547f2e93f4d926ce5c6548d68f8e66ca0509b2af
Pushpendra-Kushwaha <kushwahapushpendra6268@gmail.com>
2025-02-16 11:52:44 +0530

    first commit

---
+++ src/main/java/com/interceptor/InceptorApplication.java
@@ -0,0 +1,13 @@
+package com.interceptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InceptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InceptorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interceptor/InceptorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interceptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InceptorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
